2b0a7b783761fd9844ad5eb5525f492a8e8192b9
ddoyle98321 <ddoyle98321@gmail.com>
2017-02-08 09:33:53 -0800

    Version .5

--- Install.ps1
+++ Install.ps1
@@ -3,13 +3,15 @@
 #   
 
 
-function CheckVersion 
+function CheckVersion() 
     $computername = ($env:computername).ToUpper()
     $APV = Get-WmiObject -Name $computername -Namespace "root\cimv2\sms" -Query "SELECT * FROM SMS_InstalledSoftware" | Where-Object ARPDisplayName -Like "Adobe Photoshop*" | Select-Object -ExpandProperty ProductVersion
     $APV = $APV.ToString().Substring(0,2)
     If ($APV -eq "18")
         { 
         Write-host "`nThe latest verion is already installed. Window closing." -ForegroundColor Yellow
+        Read-Host "`nPress Enter to quit" -ForegroundColor Yellow
+        exit
         }
     elseif ($APV -ge "1" -and -le "17") 
         {
@@ -20,15 +22,22 @@ function CheckVersion
         InstallSoftware
         }
 
-function UninstallSoftware
+function UninstallSoftware()
     Write-Host "`nUninstalling old version of Adobe Creative Cloud...please be patient!" -ForegroundColor Green
     try {
-        Start-Process ".\Uninstaller\AdobeCCUninstaller.exe" -Verb runAs -Wait -ErrorAction Stop
+        Start-Process ".\Uninstaller\AdobeCCUninstaller.exe" -Verb runAs -Wait -ErrorAction SilentlyContinue
         $TempPath = Get-ChildItem -Path Env:TEMP | Select-Object value | Format-Table -HideTableHeaders
         $Result = Select-String -Path $TempPath"\AdobeCCUninstaller.log"
-
-    }
+        InstallSoftware
+        }
     catch {
         
     }
-    cs
+    
+function InstallSoftware()
+    Write-Host "`nInstalling Adobe Creative Cloud 2017...please wait." -ForegroundColor Green
+    try {
+        Start-Process ".\Installer\AdobeDesignStandard2017PCC\Build\Setup.exe" -Verb runAs -Wait -ErrorAction SilentlyContinue
+    }
+
+CheckVersion
